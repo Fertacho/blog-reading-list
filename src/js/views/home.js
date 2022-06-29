062bc4746ecf0ab69d7ec07141387445a14876ae
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-29 23:30:07 +0000

    avance

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,5 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import { Link } from 'react-router-dom'
 
 export const Home = () => (
 	<div className="text-center mt-5">
---
+++ src/js/views/planets.js
@@ -0,0 +1,3 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom"
