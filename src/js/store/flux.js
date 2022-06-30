06458d9a9338ece1931445b9d44955aea12dc1f8
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-30 00:03:59 +0000

    avance

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,19 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters:[],
+			person:[],
+			planets:[],
+			planet:[],
+			vehicles:[],
+			favorites:[],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getCharacters:()=>{
+				fetch("https://www.swapi.tech/api/people", requestOptions)
+  				.then(response => response.json())
+  				.then(result => setStore({characters:[...store.characters,result[0].url]}))
+  				.catch(error => console.log('error', error));
 			}
 		}
 	};
