c6782c223b2d55df73e151de3283772928ea0487
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-30 00:41:40 +0000

    avance

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -9,10 +9,12 @@ const getState = ({ getStore, getActions, setStore }) => {
 			favorites:[],
 		},
 		actions: {
+			
 			getCharacters:()=>{
-				fetch("https://www.swapi.tech/api/people", requestOptions)
+				const store = getStore();
+				fetch("https://www.swapi.tech/api/people")
   				.then(response => response.json())
-  				.then(result => setStore({characters:[...store.characters,result[0].url]}))
+  				.then(result => setStore({characters:[store.characters,result[0].url]}))
   				.catch(error => console.log('error', error));
 			}
 		}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
+export const Home = () => {
+	const {store,actions} = useContext(Context)
+ return (
 	<div className="text-center mt-5">
-		
+		<button onClick={actions.getCharacters}>get characters</button>
+		{store.characters.map((value,index)=>{
+return <li key={index}>{value}</li>
+		})}
 	</div>
-);
+)};
