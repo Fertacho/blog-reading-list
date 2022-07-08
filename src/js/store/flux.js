dddf7086d67b7cb21579e554deb4cca6c13129ac
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-07-08 00:35:42 +0000

    avance

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -14,7 +14,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const store = getStore();
 				fetch("https://www.swapi.tech/api/people")
   				.then(response => response.json())
-  				.then(result =>{console.log(result,"este es el fetch"), setStore({characters:[result.results]})})
+  				.then(result =>{console.log(result,"este es el fetch"), setStore({characters:[...result.results]})})
   				.catch(error => console.log('error', error));
 			}
 		}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,16 @@ import { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 
 export const Home = () => {
+
 	const {store,actions} = useContext(Context)
+	useEffect(() => {
+        actions.getCharacters();
+    }, [])
 	console.log(store.characters)
  return (
 	<div className="text-center mt-5">
-		<button onClick={actions.getCharacters}>get characters</button>
-		{store.characters.map((value,index)=>{
-return <li key={index}>{value[0].name}</li>
+		{store.characters.map((characters,index)=>{
+return <p key={index}>{characters.name}</p>
 		
 		})}
 	</div>
